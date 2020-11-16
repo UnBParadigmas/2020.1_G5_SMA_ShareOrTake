146ce5e6ae836275247f64702e3ea21ad5f24e1c
Eduardo Lima <eduardolimrib@gmail.com>
2020-11-16 19:38:04 -0300

    Modified resource class

--- src/simulation/resources/Resource.java
+++ src/simulation/resources/Resource.java
@@ -2,6 +2,7 @@ package simulation.resources;
 
 import java.util.Arrays;
 import java.util.List;
+import java.lang.Math.random;
 
 /*
  * Recurso para alimentar as criaturas
@@ -13,28 +14,58 @@ public class Resource {
 	private Integer Xpos;
 	private Integer Ypos;
 	
+	// Retorna o nome atual do recurso.
 	public String getName() {
+		// Retorna o nome atual do recurso.
 		return name;
 	}
 	
+	// Muda o nome do recurso para o argumento.
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
+	// Retorna quantia atual de comida.
 	public Integer getFoodAmount() {
 		return foodAmount;
 	}
-	
+
+	// Muda a quantia de um recurso para o argumento recebido.
 	public void setFoodAmount(Integer foodAmount) {
 		this.foodAmount = foodAmount;
 	}
+
+	// Adiciona a quantia igual ao argumento recebido ao valor atual de quantia.
+	public void addFoodAmount(Integer foodAmount) {
+		this.foodAmount += foodAmount;
+	}
 	
+	// Muda a posição para os valores fornecidos como argumento.
 	public void setPos(Integer Xpos, Integer Ypos) {
+		// Muda
 		this.Xpos = Xpos;
 		this.Ypos = Ypos;
 	}
+
+	// Muda a posição do recurso para um valor aleatório dentro dos limites.
+	public void randompos() {
+		// Valores máximos e minimos para posições X e Y de um recurso.
+		int max = 100;
+		int min = 0;
+
+		this.Xpos = (Math.random() * (max - min) + min);
+		this.Ypos = (Math.random() * (max - min) + min);
+	}
 	
+	// Retorna a posição atual do recurso.
 	public List<Integer> getPos() {
 		return Arrays.asList(this.Xpos, this.Ypos);
+	}
+
+	// Construtor parametrizado
+	public Resource(String name, Integer amount) {
+		this.name = name;
+		this.foodAmount = amount;
+		this.randompos();
 	}
 }
